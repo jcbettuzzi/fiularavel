4a9b92b496fa9610f3756365c8a0c5cbdc070c1a
jcbettuzzi <jean-charles@rbmg.com>
2025-10-16 11:21:15 +0200

    credossier

---
+++ credossiers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Il faut créer un fichier .env ( à partir de env-test par exemple)
+
+mkdir ./storage/app
+mkdir ./storage/app/public
+mkdir ./storage/framework
+mkdir ./storage/framework/cache
+mkdir ./storage/framework/cache/data
+mkdir ./storage/framework/sessions
+mkdir ./storage/framework/testing
+mkdir ./storage/framework/views
+mkdir ./storage/logs
+
+
+php artisan key:generate
+
+
