56d9aa0218b78a2094b58763b4cde88ab177354d
jcbettuzzi <jean-charles@rbmg.com>
2025-10-16 11:46:17 +0200

    credossier

--- credossiers.sh
+++ credossiers.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Il faut créer un fichier .env ( à partir de env-test par exemple)
+# attention il faut passer la commande:
+# composer update
+# pour que php artisan key focntionne
 
 mkdir ./storage/app
 mkdir ./storage/app/public
